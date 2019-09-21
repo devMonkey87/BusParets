46ade0c3f17b0ccfe4bbf7ff1879fe3d1ddb5a11
Jose A. Landete <jlandetemunoz@icloud.com>
2019-09-21 19:47:39 +0200

    Initial Commit

---
+++ BusParets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusParets
+//
+//  Created by Jose Antonio Landete Muñoz on 21/09/2019.
+//  Copyright © 2019 devMonkey87. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
