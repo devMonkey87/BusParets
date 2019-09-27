89661dc11c8ca09e4f20de320ad0b9c60de9941a
jose <joseantoniolandete@gmail.com>
2019-09-27 19:27:27 +0200

    millores

---
+++ BusParets/LolViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LolViewController.swift
+//  BusParets
+//
+//  Created by Jose Antonio Landete Muñoz on 24/09/2019.
+//  Copyright © 2019 devMonkey87. All rights reserved.
+//
+
+import UIKit
+
+class LolViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
